fc0d1544198f090b868bdc07387a6449c7464dd8
Steve <stevetuozzo@gmail.com>
2023-04-24 22:49:11 -0400

    Added inventory emojis, not useable

--- bot/commands/player/backpack.js
+++ bot/commands/player/backpack.js
@@ -26,6 +26,23 @@ module.exports = {
                 { name: `${backpack[item].name }`, value: `Quantity: ${backpack[item].amt}` }
             )
         })
-        return interaction.reply({ embeds: [backpackEmbed] });
+
+
+
+        const message = await interaction.reply({ embeds: [backpackEmbed], fetchReply: true });
+        message.react("1100250570666803200"); // back
+        message.react("1100250571669241916"); // next
+        message.react("1100250572965290104"); // 1
+        message.react("1100250573820932116"); // 2
+        message.react("1100250574735290448"); // 3
+        message.react("1100250575699976263"); // 4
+        message.react("1100250577054736495"); // 5
+        message.react("1100250578422075542"); // 6
+        message.react("1100250616061763584"); // 7
+        message.react("1100250617647206510"); // 8
+        message.react("1100250618574143519"); // 9
+        message.react("1100250619358494720"); // 10
+
+
     },
 };
